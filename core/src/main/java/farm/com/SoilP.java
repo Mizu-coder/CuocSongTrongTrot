d3adc74f29d798606f9e9d52c0f5f5cdb67e6271
Mizu <vuongdinhnammon2008@gmail.com>
2026-06-27 15:34:34 +0700

    ruongtronglua

--- core/src/main/java/farm/com/SoilP.java
+++ core/src/main/java/farm/com/SoilP.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 public class SoilP extends MyActor{
     public SoilP(float x, float y, Stage s){
         super(x,y,s);
-        textureRegion = new TextureRegion(new Texture("ruonglua.png"));
+        textureRegion = new TextureRegion(new Texture("ruonglua.jpg"));
         setSize(textureRegion.getRegionWidth()/1.25f, textureRegion.getRegionHeight()/1.25f);
     }
 }
